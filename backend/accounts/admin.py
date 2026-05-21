7f27e5695a4fbb6b594e9ca89ecfb711e2742745
SI70185430 <262584973+SI70185430@users.noreply.github.com>
2026-05-21 14:37:41 +0900

    各テーブルを管理画面に追加

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,1 +1,34 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import AppUser
+
+@admin.register(AppUser)
+class AppUserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "username",
+        "employee_id",
+        "password",
+        "is_staff",
+        "is_active",
+        "last_login",
+        "date_joined",
+        "updated_at",
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "employee_id",
+                    "is_staff",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,1 +1,40 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Book, BookCopy, Genre
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        "c_code_genre",
+        "name",
+    )
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "genre",
+        "isbn",
+        "title",
+        "author",
+        "publisher",
+        "published_date",
+        "price",
+        "cover_image_url",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+
+@admin.register(BookCopy)
+class BookCopyAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "book",
+        "status",
+        "location",
+        "purchase_date",
+        "note",
+        "created_at",
+        "updated_at",
+    )
