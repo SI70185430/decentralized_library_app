af281b1f62c59800eefbe18af8b80fe8821b3f24
SI70185430 <262584973+SI70185430@users.noreply.github.com>
2026-05-21 11:39:06 +0900

    jazzmin追加

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = environ.get("ALLOWED_HOSTS", "").split(",")
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
