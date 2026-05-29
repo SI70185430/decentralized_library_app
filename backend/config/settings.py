90bdcb20d8bc2a8c1e7ff07073be3b0e50d0e654
SI70185430 <262584973+SI70185430@users.noreply.github.com>
2026-05-29 14:58:04 +0900

    CORS / SessionAuthentication 用の設定追加

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -138,3 +138,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
